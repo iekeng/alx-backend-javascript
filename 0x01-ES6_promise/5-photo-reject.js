0a48d8c20a88bc01431ebd084e83117a88280a99
iekeng <ekeng7.eb@gmail.com>
2023-07-27 17:20:54 +0000

    fix rejected promise

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,4 +1,3 @@
 export default function uploadPhoto(filename) {
-  const promise = new Promise((reject) => reject(Error(`${filename} cannot be processed`)));
-  return promise;
+  return Promise.reject(Error(`${filename} cannot be processed`));
 }
