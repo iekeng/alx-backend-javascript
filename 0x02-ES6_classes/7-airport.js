8910bd5673bf5ff13dd7535c1872e66d6e2f6ad0
iekeng <ekeng7.eb@gmail.com>
2023-07-28 22:17:54 +0000

    airport class

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,13 @@
+export default class Airport {
+  constructor(name, code) {
+    if (typeof name !== 'string' && typeof code !== 'string') {
+      throw TypeError('name and code must of string type');
+    }
+    this._name = name;
+    this._code = code;
+  }
+
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
+}
