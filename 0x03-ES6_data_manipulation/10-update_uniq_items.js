df52e0428947403aeb4af0017926310dbf6a57c3
iekeng <ekeng7.eb@gmail.com>
2023-08-01 21:54:55 +0000

    fix error object

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,7 @@
 export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw Error('Cannot process');
+  }
   for (const [key, val] of map) {
     if (val === 1) {
       map.set(key, 100);
