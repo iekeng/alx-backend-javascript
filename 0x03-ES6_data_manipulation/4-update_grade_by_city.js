399f1dcb0d34898d00628ac2f292e1eb37a6b079
iekeng <ekeng7.eb@gmail.com>
2023-08-01 13:07:55 +0000

    updates grades by city

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,8 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  const cityStudents = students.filter((student) => student.location === city).map((student) => {
+    const item2 = newGrades.find((student2) => student.id === student2.studentId);
+
+    return { ...student, grade: item2 ? item2.grade : 'N/A' };
+  });
+  return cityStudents;
+}
