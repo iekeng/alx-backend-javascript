3e0206ebd0ee6ea585f5f030ef1f4e8eafff7850
iekeng <ekeng7.eb@gmail.com>
2023-09-21 13:26:45 +0000

    err fix

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,15 +1,16 @@
 const express = require('express');
-const app = express();
-const port = 7865;
 
-app.listen(port, () => {
-    console.log(`API available on localhost port ${port}`)
-});
+app = express();
+port = 7865;
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the payment system');
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
+  res.send('Welcome to the payment system');
+});
+
+app.listen(port, () => {
+  console.log(`API available on localhost port ${port}`);
 });
 
 module.exports = app;
--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,16 +1,12 @@
-const app = require('./api');
+const chai = require('chai');
 const request = require('request');
-const expect = require('chai').expect;
 
-describe('Index page', function(){
-  const url = 'http://localhost:7865';
-	
-  it('verify status code and content of url request to homepage', function(done){
-    resquest.get(url)
-    .then((res) => {
-    expect(res.statusCode).to.equal(200);
-    expect(res.body).to.equal('Welcome to the payment system');
-    done();
+describe('Test the Express API', () => {
+  it('should return the right results', (done) => {
+    request.get('http://localhost:7865/', (error, response, body) => {
+      chai.expect(response.statusCode).to.equal(200);
+      chai.expect(body).to.equal('Welcome to the payment system');
+      done();
     });
   });
 });
