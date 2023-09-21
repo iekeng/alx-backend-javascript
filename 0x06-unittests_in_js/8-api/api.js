6d40d1ae2d5abea18502ab1dc3488836980ace7f
iekeng <ekeng7.eb@gmail.com>
2023-09-21 13:24:59 +0000

    err fix

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 const port = 7865;
 
-app.listen(port, (error) => {
-  if(!error){
+app.listen(port, () => {
     console.log(`API available on localhost port ${port}`)
-  }
 });
 
 app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
 });
 
 module.exports = app;
