3e5d0f65fe37282520bd5ab4ba59ff8dfe4bc54e
iekeng <ekeng7.eb@gmail.com>
2023-09-21 08:40:49 +0000

    validate Util.calculatNumber

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,19 @@
+const Utils = {
+	calculateNumber: function(type, a, b) {
+    if (type === 'SUM') {
+      return Math.round(a) + Math.round(b);
+    }
+    if (type === 'SUBTRACT') {
+      return Math.round(a) - Math.round(b);
+    }
+    if (type === 'DIVIDE') {
+      const result = Math.round(a) / Math.round(b);
+      if (result === Infinity) {
+        return 'Error';
+      }
+      return result;
+    }
+  },
+};
+
+module.exports = Utils;
