a7a42796da9bc39ddd5ad0fd25a1ab7d632e0ee6
iekeng <ekeng7.eb@gmail.com>
2023-09-23 05:17:10 +0000

    read asynchronously

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+
+function getList(field, lines) {
+  const studentList = [];
+  lines.forEach((line) => {
+    const [firstName, lastName, age, currentField] = line.split(',');
+    if (firstName && lastName && age && currentField === field) {
+      studentList.push(firstName);
+    }
+  });
+  const list = studentList.join(', ');
+  return list;
+}
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    const fieldCount = {};
+    let lineCount = 0;
+
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        reject('Cannot load the database');
+      } else {
+        const fieldLines = data.trim().split('\n');
+        const lines = fieldLines.slice(1); // Use slice instead of splice to skip the header line
+
+        lines.forEach((line) => {
+          const [firstName, lastName, age, field] = line.split(',');
+          if (firstName && lastName && age && field) {
+            if (fieldCount[field]) {
+              fieldCount[field] += 1;
+            } else {
+              fieldCount[field] = 1;
+            }
+            lineCount += 1;
+          }
+        });
+
+        // Log the total number of students
+        console.log(`Number of students: ${lineCount}`);
+
+        // Log the number of students in each field
+        for (const field in fieldCount) {
+          console.log(`Number of students in ${field}: ${fieldCount[field]}. List: ${getList(field, lines)}`);
+        }
+
+        resolve(); // Resolve the promise when processing is complete
+      }
+    });
+  });
+}
+
+module.exports = countStudents;
